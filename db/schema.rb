e84b1de459e2d04d00a7b556ae8a2c823520698f
akiyuki-23 <113977720+akiyuki-23@users.noreply.github.com>
2022-10-18 18:26:14 +0900

    messagesテーブルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
+ActiveRecord::Schema.define(version: 2022_10_18_091544) do
 
-ActiveRecord::Schema.define(version: 2022_10_18_054311) do
+  create_table "messages", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "content"
+    t.bigint "user_id", null: false
+    t.bigint "room_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["room_id"], name: "index_messages_on_room_id"
+    t.index ["user_id"], name: "index_messages_on_user_id"
+  end
 
   create_table "room_users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "user_id", null: false
@@ -28,9 +37,6 @@ ActiveRecord::Schema.define(version: 2022_10_18_054311) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
-ActiveRecord::Schema.define(version: 2022_10_13_075711) do
-
-
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
     t.string "email", default: "", null: false
@@ -44,8 +50,8 @@ ActiveRecord::Schema.define(version: 2022_10_13_075711) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-
+  add_foreign_key "messages", "rooms"
+  add_foreign_key "messages", "users"
   add_foreign_key "room_users", "rooms"
   add_foreign_key "room_users", "users"
-  
 end
